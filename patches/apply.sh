5916523ea963612669ac20f40709cd7b481f9a8a
Manuel A. Ruiz Garcia <mruiz.ve@gmail.com>
2021-03-20 20:23:05 +0100

    adding one more keras patch.

--- patches/apply.sh
+++ patches/apply.sh
@@ -8,4 +8,5 @@
   patch /usr/local/lib/python3.*/dist-packages/keras/engine/saving.py saving.py.patch
   patch /usr/local/lib/python3.*/dist-packages/keras/backend/tensorflow_backend.py tensorflow_backend.py.patch
   patch /usr/local/lib/python3.*/dist-packages/keras/callbacks/tensorboard_v1.py tensorboard_v1.py.patch
+  patch /usr/local/lib/python3.6/dist-packages/tensorflow_core/python/keras/saving/hdf5_format.py hdf5_format.py.patch
 )
